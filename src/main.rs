11b25a2f0fc3ba627829fd1dc3c443a4f41470e7
Daniel Russ <druss@mail.nih.gov>
2026-09-24 17:25:49 -0400

    now using soccer cache

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,20 @@
 use std::process::ExitCode;
 
 use clap::{Parser, Subcommand, ValueEnum};
-use soccer_rs::{MODEL_CONFIG, ModelType, SoccerBuilder, SoccerPipeline};
+use soccer_cache::Cache;
+
+use crate::SoccernetVersion::V1_0_0;
+
+trait ModelInfo {
+    fn get_soccer_model(&self) -> &'static str;
+    fn get_soccer_url(&self) -> String {
+        format!(
+            "https://danielruss.github.io/soccer-models/{}",
+            self.get_soccer_model()
+        )
+    }
+    fn get_embedding_model(&self) -> &'static str;
+}
 
 #[derive(Parser, Debug)]
 #[command(
@@ -13,7 +26,10 @@ use soccer_rs::{MODEL_CONFIG, ModelType, SoccerBuilder, SoccerPipeline};
 )]
 struct Cli {
     #[command(subcommand)]
-    classifier: Classifier,
+    classifier: Option<Classifier>,
+
+    #[arg(long)]
+    clear: bool,
 }
 
 #[derive(ValueEnum, Clone, Copy, Debug, PartialEq, Eq)]
@@ -21,9 +37,17 @@ enum SoccernetVersion {
     #[value(name = "1.0.0")]
     V1_0_0,
 }
-impl std::fmt::Display for SoccernetVersion {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", self.as_ref())
+
+impl ModelInfo for SoccernetVersion {
+    fn get_soccer_model(&self) -> &'static str {
+        match self {
+            Self::V1_0_0 => "SOCcer_v3.0.0.onnx",
+        }
+    }
+    fn get_embedding_model(&self) -> &'static str {
+        match self {
+            Self::V1_0_0 => "Xenova/GIST-small-Embedding-v0",
+        }
     }
 }
 impl AsRef<str> for SoccernetVersion {
@@ -33,11 +57,28 @@ impl AsRef<str> for SoccernetVersion {
         }
     }
 }
+impl std::fmt::Display for SoccernetVersion {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.as_ref())
+    }
+}
 
 #[derive(ValueEnum, Clone, Copy, Debug, PartialEq, Eq)]
 enum ClipsVersion {
     #[value(name = "1.0.0")]
     V1_0_0,
+}
+impl ModelInfo for ClipsVersion {
+    fn get_soccer_model(&self) -> &'static str {
+        match self {
+            Self::V1_0_0 => "clips_v0.0.2.onnx",
+        }
+    }
+    fn get_embedding_model(&self) -> &'static str {
+        match self {
+            Self::V1_0_0 => "Xenova/GIST-small-Embedding-v0",
+        }
+    }
 }
 impl AsRef<str> for ClipsVersion {
     fn as_ref(&self) -> &str {
@@ -46,7 +87,6 @@ impl AsRef<str> for ClipsVersion {
         }
     }
 }
-
 impl std::fmt::Display for ClipsVersion {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "{}", self.as_ref())
@@ -75,27 +115,86 @@ impl std::fmt::Display for Classifier {
 
 fn main() -> ExitCode {
     let args = Cli::parse();
-    let config = match &args.classifier {
-        Classifier::Soccernet { version } => {
-            MODEL_CONFIG.get_config(&ModelType::SOCcerNET, version.as_ref())
+
+    if args.clear {
+        return match Cache::clear_cache("Xenova/GIST-small-Embedding-v0") {
+            Ok(()) => {
+                println!("model cache cleared");
+                ExitCode::SUCCESS
+            }
+            Err(error) => {
+                eprintln!("could not clear model cache: {error}");
+                ExitCode::FAILURE
+            }
+        };
+    }
+
+    let cache = Cache::new();
+    let cache = match cache {
+        Ok(c) => c,
+        Err(e) => {
+            eprintln!("{e}");
+            return ExitCode::FAILURE;
         }
-        Classifier::Clips { version } => {
-            MODEL_CONFIG.get_config(&ModelType::CLIPS, version.as_ref())
-        }
-    };
-    let Some(model_config) = config else {
-        eprintln!("ERROR: Unknown model/version");
-        return ExitCode::FAILURE;
     };
 
-    match SoccerPipeline::build(model_config) {
-        Ok(_) => {
-            println!("models are cached");
-            ExitCode::SUCCESS
+    let classifier = args
+        .classifier
+        .unwrap_or(Classifier::Soccernet { version: V1_0_0 });
+
+    match &classifier {
+        Classifier::Soccernet { version } => {
+            let model_id = version.get_embedding_model();
+
+            let embed_cached = Cache::is_cached(model_id);
+            let soccer_cached = Cache::is_soccer_cached(version.get_soccer_model());
+
+            if soccer_cached && embed_cached {
+                println!("{} is already cached", classifier);
+                return ExitCode::SUCCESS;
+            }
+
+            if !soccer_cached {
+                let cached_pathbuf_res = cache.get_from_url(&version.get_soccer_url());
+                if let Err(error) = cached_pathbuf_res {
+                    eprintln!("Problem caching SOCcerNET: {error}");
+                    return ExitCode::FAILURE;
+                }
+            }
+            if !embed_cached {
+                let cached_pathbuf_res = cache.cache_hf_model(model_id);
+                if let Err(error) = cached_pathbuf_res {
+                    eprintln!("Problem caching {} from Hugging Face: {error}", model_id);
+                    return ExitCode::FAILURE;
+                }
+            }
         }
-        Err(e) => {
-            eprintln!("ERROR: {}", e);
-            ExitCode::FAILURE
+        Classifier::Clips { version } => {
+            let model_id = version.get_embedding_model();
+
+            let embed_cached = Cache::is_cached(model_id);
+            let soccer_cached = Cache::is_soccer_cached(version.get_soccer_model());
+
+            if soccer_cached && embed_cached {
+                println!("{} is already cached", classifier);
+                return ExitCode::SUCCESS;
+            }
+            if !soccer_cached {
+                let cached_pathbuf_res = cache.get_from_url(&version.get_soccer_url());
+                if let Err(error) = cached_pathbuf_res {
+                    eprintln!("Problem caching CLIPS: {error}");
+                    return ExitCode::FAILURE;
+                }
+            }
+            if !embed_cached {
+                let cached_pathbuf_res = cache.cache_hf_model(model_id);
+                if let Err(error) = cached_pathbuf_res {
+                    eprintln!("Problem caching {} from Hugging Face: {error}", model_id);
+                    return ExitCode::FAILURE;
+                }
+            }
         }
-    }
+    };
+
+    ExitCode::SUCCESS
 }
